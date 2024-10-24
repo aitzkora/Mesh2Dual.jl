572b5f483fff33a27f4e212b3fa5c69983a5b087
aitzkora <marc.fuentes@aquilenet.fr>
2024-10-24 13:27:49 +0200

    cosmetical changes

--- check.jl
+++ check.jl
@@ -19,30 +19,28 @@ function check(::Type{T}, f1::String, f2::String, baseval::T = zero(T)) where {T
   end
 end
 
-
-
 if (length(ARGS) < 2)
-    println("usage : julia check.jl fileIndexedOne fileIndexedTwo [baseval]")
-    exit(-1) 
+  println("usage : julia check.jl fileIndexedOne fileIndexedTwo [baseval]")
+  exit(-1) 
 end
 
 f1 = parse_file_name(ARGS[1])
 f2 = parse_file_name(ARGS[2])
 if (length(ARGS) >= 3)
-    baseval = parse(Int64, ARGS[3])
+  baseval = parse(Int64, ARGS[3])
 end
 if (isempty(f1))
-    println(ARGS[1] * " is not in the current dir !"); exit(-1)
+  println(ARGS[1] * " is not in the current dir !"); exit(-1)
 end 
 if (isempty(f2))
-    println(ARGS[2] * " is not in the current dir !"); exit(-1)
+  println(ARGS[2] * " is not in the current dir !"); exit(-1)
 end 
 
 if (length(f1) != length(f2))
-    println("two generic filenames do not have the same number of processes"); exit(-1)
+  println("two generic filenames do not have the same number of processes"); exit(-1)
 else
-    for i=1:length(f1)
-        check(Int32, f1[i], f2[i], Int32(baseval))
-    end
-    println("****** SUCCESS *****");
+  for i=1:length(f1)
+    check(Int32, f1[i], f2[i], Int32(baseval))
+  end
+  println("****** SUCCESS *****");
 end
