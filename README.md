7a184dd172fb0dc6c2d15c82748371b4657e3598
aitzkora <marc.fuentes@aquilenet.fr>
2024-10-24 13:27:13 +0200

    update Readme

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
-# Mesh2Dual
-- prototype code to compute dual graphs (element graph) of meshes
+# Mesh2Dual.jl
+This julia package aims to make experiments with some algorithms on meshes to compute dual graphs, i.e. also called graphs of elements, to implement after the corresponding algorithm in the Scotch software. Some functions manipulating meshes and graphs could be used for others purposes
 
-# Dependances
-- to run tests you need of shared versions of `libmetis` and `libparmetis`
-and export their paths to `METIS_LIB` and `PARMETIS_LIB`
+## Compile the package
+```julia
+using Pkg
+Pkg.activate("path/to/Mesh2Dual.jl")
+using Mesh2Dual
+```
+
+## Testing
+```julia
+Pkg.test("Mesh2Dual")
+```
+
+## compute a dual graph of a mesh in parallel
+
+```bash
+mpirun -np 3 julia  dual_skotx.jl --filename mesh_oliv.mesh --shift 1
+```
+
+## How to check if two distributed graphs are equal
+
+```bash
+check.jl mesh_oliv-%r.dgr mesh_0-%r.dgr
+```
+Beware, the two dgr files must be in the working directory (or calling)
+
+# Dependencies
+- to run some tests you need of shared versions of `libmetis` and `libparmetis` exported with their paths into  `METIS_LIB` and `PARMETIS_LIB` environments variables
--- check.jl
+++ check.jl
@@ -1,7 +1,7 @@
 #!/usr/bin/env julia
 using Mesh2Dual
 
-function check(f1, f2)
+function check(::Type{T}, f1::String, f2::String, baseval::T = zero(T)) where {T<:Integer}
   println("comparing $f1 and $f2")
   io1 = open(f1)
   io2 = open(f2)
@@ -12,7 +12,7 @@ function check(f1, f2)
   close(io1)
   close(io2)
   for i=1:length(adj1)
-    if (sort(adj1[i]) != sort(adj2[i]))
+    if (sort(adj1[i]) != sort(adj2[i]) .+ baseval)
       println("$f1 and $f2 are differents")
       exit(-1)
     end
@@ -22,24 +22,27 @@ end
 
 
 if (length(ARGS) < 2)
-    println("usage : julia check.jl fileIndexedOne fileIndexedTwo")
+    println("usage : julia check.jl fileIndexedOne fileIndexedTwo [baseval]")
     exit(-1) 
 end
 
 f1 = parse_file_name(ARGS[1])
 f2 = parse_file_name(ARGS[2])
+if (length(ARGS) >= 3)
+    baseval = parse(Int64, ARGS[3])
+end
 if (isempty(f1))
-    println(ARGS[1]* " is not in the current dir !"); exit(-1)
+    println(ARGS[1] * " is not in the current dir !"); exit(-1)
 end 
 if (isempty(f2))
-    println(ARGS[2]* " is not in the current dir !"); exit(-1)
+    println(ARGS[2] * " is not in the current dir !"); exit(-1)
 end 
 
 if (length(f1) != length(f2))
     println("two generic filenames do not have the same number of processes"); exit(-1)
 else
     for i=1:length(f1)
-        check(f1[i], f2[i])
+        check(Int32, f1[i], f2[i], Int32(baseval))
     end
     println("****** SUCCESS *****");
 end
